ddc5e61d0008c5c01c66f3296ae5fc7cf4d3dd8e
Sophia Gigliotti <skgigliotti@gmail.com>
2020-12-13 13:07:11 -0800

    Basic word model

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,5 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+words = Word.create([{ spelling: 'tranquilo', language: 'Spanish', english: 'quiet' }, { spelling: 'しずかな', language: 'Japanese', english: 'quiet'}, { spelling: 'tranquillo', language: 'Italian', english: 'quiet'}])
